212664797b4dee73a23b9f939653b8a12814730d
cais <cais@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-11 18:38:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/bns-featured-category/trunk@204306 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bns-featured-category.php
+++ bns-featured-category.php
@@ -3,9 +3,30 @@
 Plugin Name: BNS Featured Category
 Plugin URI: http://buynowshop.com/plugins/bns-featured-category/
 Description: Plugin with multi-widget functionality that displays most recent posts from specific category or categories (set with user options). Also includes user options to display: Author and meta details; comment totals; post categories; post tags; and either full post, excerpt, or your choice of the amount of words (or any combination).  
-Version: 1.6.2.1
+Version: 1.6.2.2
 Author: Edward Caissie
 Author URI: http://edwardcaissie.com/
+*/
+
+/*
+**
+* Copyright 2009, 2010 Edward Caissie 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+* Plugin Changelog: see readme.txt
+**
 */
 
 global $wp_version;
